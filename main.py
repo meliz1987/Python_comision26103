88f536b95eb7e030b44acef8c1fe78c2b2cfb6ed
meliz1987 <meliz1987@gmail>
2026-06-29 19:15:05 -0300

    Implementar CRUD parcial con SQLite

--- main.py
+++ main.py
@@ -27,7 +27,61 @@ def crear_tabla():
 
 crear_tabla()
 
-products = []
+def agregar_producto(nombre, descripcion, cantidad, precio, categoria):
+    conexion = conectar()
+    cursor = conexion.cursor()
+
+    cursor.execute(
+        """
+        INSERT INTO productos
+        (nombre, descripcion, cantidad, precio, categoria)
+        VALUES (?, ?, ?, ?, ?)
+        """,
+        (nombre, descripcion, cantidad, precio, categoria)
+    )
+
+    conexion.commit()
+    conexion.close()
+
+def obtener_productos():
+    conexion = conectar()
+    cursor = conexion.cursor()
+
+    cursor.execute("SELECT * FROM productos")
+    productos = cursor.fetchall()
+
+    conexion.close()
+
+    return productos
+def buscar_producto_por_id(id_producto):
+    conexion = conectar()
+    cursor = conexion.cursor()
+
+    cursor.execute(
+        "SELECT * FROM productos WHERE id = ?",
+        (id_producto,)
+    )
+
+    producto = cursor.fetchone()
+
+    conexion.close()
+
+    return producto
+def eliminar_producto(id_producto):
+    conexion = conectar()
+    cursor = conexion.cursor()
+
+    cursor.execute(
+        "DELETE FROM productos WHERE id = ?",
+        (id_producto,)
+    )
+
+    conexion.commit()
+    filas_afectadas = cursor.rowcount
+    conexion.close()
+
+    return filas_afectadas
+
 
 while True:
     print("\n" + "=" * 50)
@@ -48,62 +102,82 @@ while True:
             productName = input("Ingrese el nombre: ").strip()
             if productName == "":
                 print("⚠️ El nombre del producto no puede estar vacío.")
+            productDescription = input("Ingrese la descripción: ")
         productCategory = input("Ingrese la categoría: ")
-        productPrice = input("Ingrese el precio (sin centavos): $ ")
+        productQuantity = int(input("Ingrese la cantidad: "))
+        productPrice = float(input("Ingrese el precio: $ "))
 
-        products.append([productName, productCategory, productPrice])
-        print(f"\n  ✅ Producto {productName} agregado correctamente.")
+        agregar_producto(
+            productName,
+            productDescription,
+            productQuantity,
+            productPrice,
+            productCategory
+        )
+
+        print(f" \n✅ Producto {productName} agregado correctamente.")        
 
     elif option == "2":
         print("\n *** 📝 Lista de Productos *** ")
-        if len(products) == 0:
+
+        productos = obtener_productos()
+
+        if len(productos) == 0:
             print("🙁 No hay productos registrados")
         else:
             print(
-                f"{'n°':<2} {'Nombre de Producto':<20}{'Categoria':<15}{'Precio':<10}"
+                f"{'ID':<5}{'Nombre':<20}{'Cantidad':<10}{'Precio':<12}{'Categoría':<15}"
             )
-            print("-" * 50)
-            for i in range(len(products)):
-                print(
-                    f"{i+1:<2}{products[i][0]:<20}{products[i][1]:<15}{products[i][2]:<10}"
-                )
-            print("-" * 50)
-    elif option == "3":
-         print("\n *** 🔎 Buscar Productos *** ")
-         productToSearch = input ("Ingrese el nombre del producto a buscar: ")
-         foundProduct = False
+            print("-" * 65)
 
-         for i in range (len(products)):
-             if products[i][0].lower() == productToSearch.lower():
-                 print(f" 🕵🏻‍♀️ Producto encontrado en el indice {i+1}")
-                 print(f"Nombre:{products [i][0]}")
-                 print(f"Categoría:{products [i][1]}")
-                 print(f"Precio:$ {products [i][2]}")
-                 foundProduct = True
-                 break
-         if not foundProduct:
-            print(f"\n  No se encontró el producto {productToSearch}")
+            for producto in productos:
+                print(
+                    f"{producto[0]:<5}"
+                    f"{producto[1]:<20}"
+                    f"{producto[3]:<10}"
+                    f"${producto[4]:<11.2f}"
+                    f"{producto[5]:<15}"
+                )
+
+            print("-" * 65)
+    elif option == "3":
+        print("\n *** 🔎 Buscar Productos *** ")
+
+        try:
+            id_producto = int(input("Ingrese el ID del producto: "))
+
+            producto = buscar_producto_por_id(id_producto)
+
+            if producto:
+                print(" \n 🕵🏻‍♀️ Producto encontrado:")
+                print(f"ID: {producto[0]}")
+                print(f"Nombre: {producto[1]}")
+                print(f"Descripción: {producto[2]}")
+                print(f"Cantidad: {producto[3]}")
+                print(f"Precio: $ {producto[4]}")
+                print(f"Categoría: {producto[5]}")
+            else:
+                print("❌ No se encontró un producto con ese ID.")
+
+        except ValueError:
+            print("❌ Debe ingresar un número.")
 
     elif option == "4":
-         print("\n *** 🗑️ Eliminar Productos *** ")
-         if len(products) == 0:
-            print("No hay productos para eliminar")
-         else:
-            print("Productos Disponibles")
-            for i in range (len(products)):
-                print(f"{i+1}. {products[i][0]} - {products[i][1]} - {products[i][2]}")
+        print("\n *** 🗑️ Eliminar Productos *** ")
 
-            indexProduct = input("Ingrese el número de producto a eliminar: ")
-            try:
-                ipr = int(indexProduct) - 1
+        try:
+            id_producto = int(input("Ingrese el ID del producto a eliminar: "))
 
-                if ipr >=0 and ipr < len(products):
-                    deleted_products = products.pop(ipr)
-                    print(f"✅ Producto { deleted_products[0]} eliminado correctamente")
-                else:
-                    print("❌ Índice no válido")
-            except ValueError:
-                print("❌ Entrada no válida. Por favor, ingrese un número.")
+            producto = buscar_producto_por_id(id_producto)
+
+            if producto:
+                eliminar_producto(id_producto)
+                print(f"✅ Producto '{producto[1]}' eliminado correctamente.")
+            else:
+                print("❌ No existe un producto con ese ID.")
+
+        except ValueError:
+            print("❌ Debe ingresar un número.")
 
     elif option == "5":
         print("☺️ Gracias por usar el sistema de gestión de Productos ")
